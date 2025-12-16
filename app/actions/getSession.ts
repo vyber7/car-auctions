90789cb5ec5090978db557be540b8045e0014e2b
VB <vb_2.0@pm.me>
2025-12-16 09:13:19 -0500

    Auth route updated

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../libs/auth-config";
+import { authOptions } from "@/app/libs/auth-config";
 
 export async function getSession() {
   const session = await getServerSession(authOptions);
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,8 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/app/libs/auth-config";
 
-export const { GET, POST } = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+
+//export const { GET, POST } = NextAuth(authOptions);
