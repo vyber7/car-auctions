795469f138ebd0365eb2a95cb98be13ea5d45773
VB <vb_2.0@pm.me>
2026-02-02 12:36:00 -0500

    description updated

--- app/listing/[id]/components/Description.tsx
+++ app/listing/[id]/components/Description.tsx
@@ -6,10 +6,6 @@ const Description: React.FC<DescriptionProps> = ({ description }) => {
   return (
     <div className="p-2 md:p-4 text-sm md:text-base border shadow-md rounded-md shadow-gray-400 bg-white">
       <p>{description}</p>
-      <br />
-      <p>{description}</p>
-      <br />
-      <p>{description}</p>
     </div>
   );
 };
